2438ba3b3481976c20e34a6f6e9b2962ca477fda
Jim Hough <teachlife@outlook.com>
2019-05-01 21:32:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  @@items = []
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  def add_item (item, price, quantity = 1)
+    @total += (price*quantity)
+    @@items << item
+  end
+  def apply_discount
+    @total = @total*
+end
